23807f0be567cc179d1043a157c922d444ccd22a
Vanya <vanya121994@gmail.com>
2014-05-16 21:58:59 +0400

    Release Server Version

--- src/ServerPackage/Server.java
+++ src/ServerPackage/Server.java
@@ -6,7 +6,9 @@ import java.util.LinkedList;
 
 public class Server {
 	ServerSocket Ssocket;
-	LinkedList<InetAddress> addresses;
+	LinkedList<InetAddress> forFiveAddresses;
+	LinkedList<InetAddress> forSixAddresses;
+	LinkedList<InetAddress> forSevenAddresses;
 	int port = 10101;
 	
 	InputStream sin;
@@ -37,29 +39,77 @@ public class Server {
 			out = new DataOutputStream(sout);
 			
 			
-			addresses = new LinkedList<InetAddress>();
+			forFiveAddresses = new LinkedList<InetAddress>();
+			forSixAddresses = new LinkedList<InetAddress>();
+			forSevenAddresses = new LinkedList<InetAddress>();
 			Integer n;
 			while ((n = in.readInt()) != null)
 			{
 				switch (n)
 				{
 				case 0:
+					Integer i = in.readInt();
 					InetAddress newAddress = InetAddress.getByName(in.readUTF());
-					addresses.add(newAddress);
+					switch (i)
+					{
+					case 5:
+						forFiveAddresses.add(newAddress);
+						break;
+					case 6:
+						forSixAddresses.add(newAddress);
+						break;
+					case 7:
+						forSevenAddresses.add(newAddress);
+						break;
+					}
+					
 					System.out.println("Server got" + newAddress.toString());
 					break;
 				case 1:
-					if (addresses.isEmpty()) 
+					Integer i2 = in.readInt();
+					switch (i2)
 					{
-						out.writeUTF("Error");
-						System.out.println("Error");
+						case 5:
+							if (forFiveAddresses.isEmpty()) 
+							{
+								out.writeUTF("Error");
+								System.out.println("Error");
+							}
+							else 
+							{
+								out.writeUTF(forFiveAddresses.getFirst().getHostAddress());
+								System.out.println("Server send " + forFiveAddresses.getFirst().getHostAddress());
+								forFiveAddresses.removeFirst();
+							}
+							break;
+						case 6:
+							if (forSixAddresses.isEmpty()) 
+							{
+								out.writeUTF("Error");
+								System.out.println("Error");
+							}
+							else 
+							{
+								out.writeUTF(forSixAddresses.getFirst().getHostAddress());
+								System.out.println("Server send " + forSixAddresses.getFirst().getHostAddress());
+								forSixAddresses.removeFirst();
+							}
+							break;
+						case 7:
+							if (forSevenAddresses.isEmpty()) 
+							{
+								out.writeUTF("Error");
+								System.out.println("Error");
+							}
+							else 
+							{
+								out.writeUTF(forSevenAddresses.getFirst().getHostAddress());
+								System.out.println("Server send " + forSevenAddresses.getFirst().getHostAddress());
+								forSevenAddresses.removeFirst();
+							}
+							break;
 					}
-					else 
-					{
-						out.writeUTF(addresses.getFirst().getHostAddress());
-						System.out.println("Server send " + addresses.getFirst().getHostAddress());
-						addresses.removeFirst();
-					}
+					
 					break;
 				case 2:
 					waitForConnections(socket);
